24d8d78ab968d280fef720205b821b62e465c689
LaoCC <fazo@qq.com>
2016-11-29 03:21:07 +0800

    Signed-off-by: LaoCC <fazo@qq.com>

--- public/yar/client.php
+++ public/yar/client.php
@@ -2,10 +2,6 @@
 $url = 'http://yaf.kaibuy.top/yar/server.php';
 
 $client = new Yar_Client($url);
-$client instanceof api and 1;
+$client instanceof RegisterApi and 1;
 
-var_dump($client->test(time(), 123));
-
-async($url, [], function ($val) {
-    echo $val;
-});
+var_dump($client->login(time(), 123));
--- public/yar/server.php
+++ public/yar/server.php
@@ -1,16 +1,35 @@
 <?php
 
-class api
+class RegisterApi
 {
-
-    public function test($val, $time)
+    /**
+     * 测试接口
+     * @param int $val 值1
+     * @param int $time 时间
+     * @return string 结果
+     * 说明
+     */
+    public function login($val, $time)
     {
 //        fastcgi_finish_request();
 //        sleep(2);
-        $value = ['val' => $val, 'i' => $time];
+        $value = ['val' => $val, 'register' => $time];
+        return json_encode($value);
+    }
+
+    /**
+     * 测试接口
+     * @param int $val 值1
+     * @param int $time 时间
+     * @return string 结果
+     */
+    public function register($val, $time)
+    {
+//        fastcgi_finish_request();
+//        sleep(2);
+        $value = ['val' => $val, 'register' => $time];
         return json_encode($value);
     }
 }
 
-$sev = new Yar_Server(new api());
-$sev->handle();
+(new Yar_Server(new RegisterApi()))->handle();
